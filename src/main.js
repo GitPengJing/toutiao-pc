e190c7ed1aee4f5681998c8108820673fd7b6f2a
PengBillion <954483281@qq.com>
2020-02-29 23:03:02 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui' // 引入element
+import 'element-ui/lib/theme-chalk/index.css' // 引入element样式
+import '@/styles/index.less' // 引入初始化样式
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   router,
