92f86d7fd6a3ddb76491ae5617db28088d6d47fc
PengBillion <954483281@qq.com>
2020-03-02 16:25:09 +0800

    表单校验准备

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,21 +7,24 @@
               <img src="../../assets/img/logo_index.png" alt="">
           </div>
           <!-- 表单 方便获取数据 -->
-          <el-form style="margin-top:20px">
+          <!-- moddl 绑定表单数据对象 -->
+          <!-- rules 绑定数据校验规则 -->
+          <el-form :model="loginForm" :rules="rules" style="margin-top:20px">
               <!-- 表单容器 -->
-              <el-form-item>
+              <!-- prop绑定定义规则的字段 -->
+              <el-form-item prop="mobile">
                   <!-- 手机号 -->
-                  <el-input placeholder="请输入手机号"></el-input>
+                  <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
               </el-form-item>
-              <el-form-item>
+              <el-form-item prop="code">
                   <!-- 验证码 -->
-                  <el-input placeholder="请输入验证码" style="width:60%"></el-input>
+                  <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:60%"></el-input>
                   <!-- 获取验证码 -->
                   <el-button style="float:right">获取验证码</el-button>
               </el-form-item>
-              <el-form-item>
+              <el-form-item prop="checkbox">
                   <!-- 勾选条款 -->
-                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+                  <el-checkbox v-model="loginForm.checkbox">我已阅读并同意用户协议和隐私条款</el-checkbox>
               </el-form-item>
               <el-form-item>
                   <!-- 登录按钮 -->
@@ -34,7 +37,22 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checkbox: false // 用户协议
+      },
+      loginRules: {
+        // required 为true则该项为必选项
+        // message提示信息
+        mobile: [],
+        code: [],
+        checkbox: []
+      }
+    }
+  }
 }
 </script>
 // lang属性定义编写规则 scoped只对当前生效css样式 否则默认是全局的
@@ -59,7 +77,7 @@ export default {
             }
         };
         .el-form-item:nth-child(2){
-            margin-bottom: 0;
+            margin-bottom: 10px;
         };
         .el-form-item:nth-child(3){
             margin-bottom: 10px;
