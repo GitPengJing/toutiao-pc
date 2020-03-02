ed40deddad6e70c79591b4d5b2f75278366dbb9d
PengBillion <954483281@qq.com>
2020-03-02 18:24:55 +0800

    主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-      <h1>我是主页</h1>
-  </div>
+<!-- 布局容器 -->
+<el-container>
+  <!-- 侧边导航 -->
+    <el-aside>
+        <h1>我是左边的</h1>
+    </el-aside>
+  <!-- 右边主体 -->
+    <el-container>
+      <!-- 头部 -->
+        <el-header>
+            <h1>我是头部</h1>
+        </el-header>
+      <!-- 主体部分 -->
+        <el-main>
+            <h1>我是主体</h1>
+            <router-view></router-view>
+        </el-main>
+    </el-container>
+</el-container>
 </template>
 
 <script>
