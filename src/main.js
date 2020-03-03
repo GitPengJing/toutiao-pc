c7867daaca8151fd4e4097762a31849536da4984
PengBillion <954483281@qq.com>
2020-03-03 23:55:34 +0800

    统一全局注册插件的使用方式Vue.use

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import ElementUI from 'element-ui' // 引入element
 import 'element-ui/lib/theme-chalk/index.css' // 引入element样式
 import '@/styles/index.less' // 引入初始化样式
 import axios from 'axios' // 引入axios
+import Components from '@/components'
 
+Vue.use(Components) // 注册全局自定义组件
 // 设置默认请求头地址
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 // axios给全局
