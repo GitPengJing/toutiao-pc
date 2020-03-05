34fc9a77874c51d6a257bb6e6c0377e98bd41441
PengBillion <954483281@qq.com>
2020-03-06 02:13:13 +0800

    评论数据加载到页面及状态过滤

--- src/components/common/bread-crumb.vue
+++ src/components/common/bread-crumb.vue
@@ -3,10 +3,10 @@
 <el-breadcrumb separator-class="el-icon-arrow-right">
     <!-- el-bread-item相当于router-link -->
     <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
-    <el-breadcrum-item>
+    <el-breadcrumb-item>
         <!-- 用来放置首页的下一级 是动态的所以用插槽 -->
         <slot name="title"></slot>
-    </el-breadcrum-item>
+    </el-breadcrumb-item>
 </el-breadcrumb>
 </template>
 
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -8,11 +8,15 @@
       </bread-crumb>
       <el-table :data="list">
           <!-- lable定义表格的列名 -->
-          <el-table-column label="标题" width="600"></el-table-column>
-          <el-table-column label="评论状态"></el-table-column>
-          <el-table-column label="总评论数"></el-table-column>
-          <el-table-column label="粉丝评论数"></el-table-column>
-          <el-table-column label="操作"></el-table-column>
+          <el-table-column prop="title" label="标题" width="500"></el-table-column>
+          <!-- el-table不显示布尔值 用formatter 格式化 -->
+          <el-table-column :formatter="status" prop="comment_status" label="评论状态"></el-table-column>
+          <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+          <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+          <el-table-column label="操作">
+              <el-button type="text">修改</el-button>
+              <el-button type="text">打开评论</el-button>
+          </el-table-column>
       </el-table>
   </el-card>
 </template>
@@ -23,10 +27,36 @@ export default {
     return {
       list: []
     }
+  },
+  methods: {
+    status (row, column, cellValue, index) {
+      // row表示该行所有数据
+      // column表示该列的信息
+      // cellValue表示当前单元格的值
+      // index表示下标
+      return cellValue ? '正常' : '关闭'
+    },
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: {
+          response_type: 'comment' // 传comment 返回用于评论管理的字段
+        },
+        method: 'get'
+      }).then(res => {
+        // console.log(res)
+        this.list = res.data.results
+      })
+    }
+  },
+  created () {
+    this.getComment()
   }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+    .el-button{
+        font-size: 12px;
+    }
 </style>
