e683b8cc22cf67be8eb668a84702e926f8a9db82
PengBillion <954483281@qq.com>
2020-03-06 01:06:40 +0800

    面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,21 @@
+<template>
+<!-- 利用elementU封装面包屑组件  separator是分割符-->
+<el-breadcrumb separator-class="el-icon-arrow-right">
+    <!-- el-bread-item相当于router-link -->
+    <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+    <el-breadcrum-item>
+        <!-- 用来放置首页的下一级 是动态的所以用插槽 -->
+        <slot name="title"></slot>
+    </el-breadcrum-item>
+</el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
