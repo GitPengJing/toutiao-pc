eff554051a6e83fa22e0770d0aa53d660b2cb014
PengBillion <954483281@qq.com>
2020-03-07 20:47:26 +0800

    素材管理-预览

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,55 +1,66 @@
 <template>
   <el-card>
-      <!-- 面包屑组件作为el-card的头 -->
-      <bread-crumb slot="header">
-         <template slot="title">
-             素材管理
-         </template>
-      </bread-crumb>
-      <!-- 上传图片 -->
-      <el-row type="flex" justify="end">
-          <!-- action不传报错 http-request为自定义上传方法-->
-          <el-upload action="" :http-request="uploadImg">
-              <el-button type="primary">上传图片<i class="el-icon-upload el-icon--right"></i></el-button>
-          </el-upload>
-      </el-row>
-      <!-- tabs标签页 v-model 绑定选中标签的name-->
-      <el-tabs v-model="activeName" @tab-click='changeTabs'>
-          <!-- 全部素材页 返回的全部内容放这里 -->
-          <el-tab-pane label="全部" name="all">
-            <div class="imgList">
-              <el-card class="imgCard" v-for="item in list" :key="item.id">
-                  <!-- 显示图片 -->
-                  <img :src="item.url" alt="">
-                  <!-- 删除和收藏 -->
-                  <el-row class="act" type="flex" align="middle" justify="space-around">
-                      <i class="el-icon-s-promotion" :style="{color: item.is_collected ? 'rgb(141, 1, 255)': 'black'}" @click="collectImg(item)"></i>
-                      <i class="el-icon-delete-solid" @click="delImg(item.id)"></i>
-                  </el-row>
-              </el-card>
-            </div>
-          </el-tab-pane>
-          <!-- 收藏素材页  收藏的内容放这里 -->
-          <el-tab-pane label="收藏" name="collect">
-            <div class="imgList">
-              <el-card class="imgCard" v-for="item in list" :key="item.id">
-                  <!-- 显示图片 -->
-                  <img :src="item.url" alt="">
-              </el-card>
-            </div>
-          </el-tab-pane>
-      </el-tabs>
-         <!-- 分页组件 -->
-         <el-row style="height:60px" type="flex" align="middle" justify="center">
-             <el-pagination background layout="prev,pager,next"
-             :total="page.total"
-             :current-page="page.currentPage"
-             :page-size="page.pageSize"
-             @current-change="changePage"
-             >
-
-             </el-pagination>
-         </el-row>
+    <!-- 面包屑组件作为el-card的头 -->
+    <bread-crumb slot="header">
+       <template slot="title">
+           素材管理
+       </template>
+    </bread-crumb>
+    <!-- 上传图片 -->
+    <el-row type="flex" justify="end">
+        <!-- action不传报错 http-request为自定义上传方法-->
+        <el-upload action="" :http-request="uploadImg" :show-file-list='false'>
+            <el-button type="primary">上传图片<i class="el-icon-upload el-icon--right"></i></el-button>
+        </el-upload>
+    </el-row>
+    <!-- tabs标签页 v-model 绑定选中标签的name-->
+    <el-tabs v-model="activeName" @tab-click='changeTabs'>
+        <!-- 全部素材页 返回的全部内容放这里 -->
+        <el-tab-pane label="全部" name="all">
+          <div class="imgList">
+            <el-card class="imgCard" v-for="(item,index) in list" :key="item.id">
+                <!-- 显示图片 -->
+                <img :src="item.url" alt="" @click="openDialog(index)">
+                <!-- 删除和收藏 -->
+                <el-row class="act" type="flex" align="middle" justify="space-around">
+                    <i class="el-icon-s-promotion" :style="{color: item.is_collected ? 'rgb(141, 1, 255)': 'black'}" @click="collectImg(item)"></i>
+                    <i class="el-icon-delete-solid" @click="delImg(item.id)"></i>
+                </el-row>
+            </el-card>
+          </div>
+        </el-tab-pane>
+        <!-- 收藏素材页  收藏的内容放这里 -->
+        <el-tab-pane label="收藏" name="collect">
+          <div class="imgList">
+            <el-card class="imgCard" v-for="(item,index) in list" :key="item.id">
+                <!-- 显示图片 -->
+                <img :src="item.url" alt="" @click="openDialog(index)">
+            </el-card>
+          </div>
+        </el-tab-pane>
+    </el-tabs>
+    <!-- 分页组件 -->
+    <el-row style="height:60px" type="flex" align="middle" justify="center">
+        <el-pagination background layout="prev,pager,next"
+        :total="page.total"
+        :current-page="page.currentPage"
+        :page-size="page.pageSize"
+        @current-change="changePage">
+        </el-pagination>
+    </el-row>
+    <!-- 图片预览幻灯片效果 -->
+    <!-- visible属性控制显示隐藏 -->
+    <!-- @close控制dialog预览框的关闭 @opened="presentOpen"-->
+    <el-dialog  :visible="dialogVisible" @close="dialogVisible = false">
+      <!-- 图片走马灯 -->
+      <!-- autoplay表示是否自动播放 -->
+      <el-carousel height="400px" :autoplay="false" ref="presentImg">
+        <!-- 循环列表，展示每一项 -->
+        <el-carousel-item v-for="item in list" :key="item.id">
+          <img style="width:100%;height:100%" :src="item.url" alt="">
+        </el-carousel-item>
+      </el-carousel>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -57,6 +68,8 @@
 export default {
   data () {
     return {
+      dialogVisible: false, // dialog预览框显示或隐藏
+      // clickIndex: -1,
       page: {
         total: 0, // 总页数 初始值0
         currentPage: 1, // 当前点击的页数 初始值1
@@ -67,6 +80,15 @@ export default {
     }
   },
   methods: {
+    // presentOpen () {
+
+    // },
+    // 定义点击图片打开dialog方法
+    openDialog (index) {
+      // this.clickIndex = index // 将点击的图片的索引给clickIndex
+      this.dialogVisible = true // 打开dialog预览框
+      this.$refs.presentImg.setActiveItem(index)
+    },
     //   定义标签页改变事件
     changeTabs () {
       // 改变标签页,重新获取资源
